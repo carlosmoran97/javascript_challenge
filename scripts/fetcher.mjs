7d94bd481bcde7f35fb1342bb1d31e10c33e5e73
Carlos Morán <moran_krlos@hotmail.com>
2019-11-16 10:09:13 -0600

    using async and await to fetch all the user info

--- scripts/fetcher.mjs
+++ scripts/fetcher.mjs
@@ -1,40 +1,37 @@
 import {User, Post} from './models.mjs';
 
-export function fetchUsers(callback){
+export async function fetchUsers(callback){
     // api urls
     const JSONPLACEHOLDER = "https://jsonplaceholder.typicode.com";
     const GENDERIZE = "https://api.genderize.io";
     const JOESCHMOE = "https://joeschmoe.io/api/v1";
 
     // fetching users
-    $.ajax({
-        url: `${JSONPLACEHOLDER}/users`,
-        success: function(data){
-            var users = data.map(function(item){
-                return new User(item.id, item.name);
-            });
-            // get gender and posts for every user
-            users.forEach(function(user){
-                $.ajax({
-                    url: `${GENDERIZE}/?name=${user.getFirstName()}`,
-                    success: function(data){
-                        const GENDER = data.gender;
-                        user.setGender(data.gender);
-                        // set photoUrl based on gender
-                        user.setPhotoUrl(GENDER == "female" ? `${JOESCHMOE}/josephine` : `${JOESCHMOE}/jacques`);
-                    }
-                });
-                $.ajax({
-                    url: `${JSONPLACEHOLDER}/posts/?userId=${user.id}`,
-                    success: function(data){
-                        data.forEach(function(post){
-                            var userPost = new Post(post.title, post.body);
-                            user.addPost(userPost);
-                        });
-                    }
-                });
-            });
-            callback(users);
-        }
+    var users = (await $.ajax({
+        url: `${JSONPLACEHOLDER}/users`
+    })).map(function(item){
+        return new User(item.id, item.name);
     });
+    // get gender and posts for every user
+    for(var i = 0; i < users.length; i++){
+        var user = users[i];
+        const genderData = await $.ajax({
+            url: `${GENDERIZE}/?name=${user.getFirstName()}`
+        });
+        const GENDER = genderData.gender;
+        user.setGender(GENDER);
+        // set photoUrl based on gender
+        user.setPhotoUrl(GENDER == "female" ? `${JOESCHMOE}/josephine` : `${JOESCHMOE}/jacques`);
+        const userPosts = await $.ajax({
+            url: `${JSONPLACEHOLDER}/posts/?userId=${user.id}`
+        });
+        userPosts.forEach(function(post){
+            var userPost = new Post(post.title, post.body);
+            user.addPost(userPost);
+        });
+
+        // sending user data
+        callback(user);
+    }
+    
 }

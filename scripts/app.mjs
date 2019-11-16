b2c4c62ff0ba50927cc29de765150462e2152028
Carlos Morán <moran_krlos@hotmail.com>
2019-11-16 10:10:07 -0600

    displaying the users info

--- scripts/app.mjs
+++ scripts/app.mjs
@@ -1,2 +1,20 @@
 import {fetchUsers} from "./fetcher.mjs";
-fetchUsers(console.log);
+
+var innerHTML = "";
+
+fetchUsers(function(user){
+    var contentDiv = document.querySelector("div.content");
+    innerHTML += `
+        <div class="user">
+            <img class="user--img" src="${user.photoUrl}"/>
+            <div class="user--name--container"><h3 class="user--name">${user.name}</h3></div>
+            <div class="user--posts">`;
+        for(var i = 0; i < user.posts.length; i++){
+            var post = user.posts[i];
+            innerHTML += `<div class="post--title" style="${i%2!=0?"color: red;":""}">${post.title}</div>`
+        }
+        innerHTML +=`</div>
+        </div>
+    `;
+    contentDiv.innerHTML = innerHTML;
+});
